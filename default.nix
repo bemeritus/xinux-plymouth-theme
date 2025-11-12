f06d323f7658d1a7431d5dc75c6dcf6248e1387d
BeMeritus <bemerituss@gmail.com>
2025-11-12 15:05:07 +0500

    chore: added default.nix

---
+++ default.nix
@@ -0,0 +1,53 @@
+{
+  stdenv,
+  fetchurl,
+  lib,
+  unzip,
+  # To select only certain themes, pass `selected_themes` as a list of strings.
+  # reference ./shas.nix for available themes
+  selected_themes ? [ ],
+}:
+let
+  version = "1.0";
+  # this file is generated via ./update.sh
+  # borrowed from pkgs/data/fonts/nerdfonts
+  themeShas = import ./shas.nix;
+  knownThemes = builtins.attrNames themeShas;
+  selectedThemes =
+    if (selected_themes == [ ]) then
+      knownThemes
+    else
+      let
+        unknown = lib.subtractLists knownThemes selected_themes;
+      in
+      if (unknown != [ ]) then
+        throw "Unknown theme(s): ${lib.concatStringsSep " " unknown}"
+      else
+        selected_themes;
+  srcs = lib.lists.forEach selectedThemes (
+    name:
+    (fetchurl {
+      url = themeShas.${name}.url;
+      sha256 = themeShas.${name}.sha;
+    })
+  );
+in
+stdenv.mkDerivation {
+  pname = "xinux-plymouth-theme";
+  inherit version srcs;
+
+  nativeBuildInputs = [
+    unzip
+  ];
+
+  sourceRoot = ".";
+  unpackCmd = "tar xzf $curSrc";
+
+  installPhase = ''
+    mkdir -p $out/share/plymouth/themes
+    for theme in ${toString selectedThemes}; do
+      mv $theme $out/share/plymouth/themes/$theme
+    done
+    find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@\/usr\/@$out\/@" {} \;
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -1,58 +1,19 @@
-
 {
-  description = "Flake for adi1090x Plymouth themes (with optional selected themes)";
+  description = "Flake for Xinux plymouth";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-
-      selectedThemes = [ "mac_style"];
-
-      themeShas = import ./shas.nix;
-      knownThemes = builtins.attrNames themeShas;
-      selected =
-        if (selectedThemes == [ ]) then
-          knownThemes
-        else
-          let
-            unknown = pkgs.lib.subtractLists knownThemes selectedThemes;
-          in
-          if (unknown != [ ]) then
-            throw "Unknown theme(s): ${pkgs.lib.concatStringsSep " " unknown}"
-          else
-            selectedThemes;
-
-      srcs = pkgs.lib.lists.forEach selected (
-        name:
-        pkgs.fetchurl {
-          url = themeShas.${name}.url;
-          sha256 = themeShas.${name}.sha;
-        }
-      );
-    in {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        pname = "xinux-plymouth-theme";
-        version = "1.0";
-        inherit srcs;
-
-        nativeBuildInputs = [ pkgs.unzip ];
-
-        sourceRoot = ".";
-        unpackCmd = "tar xzf $curSrc";
-
-        installPhase = ''
-          mkdir -p $out/share/plymouth/themes
-          for theme in ${toString selected}; do
-            mv $theme $out/share/plymouth/themes/$theme
-          done
-          find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@/usr/@$out/@" {} \;
-        '';
-
+      pkgs = import nixpkgs {
+        inherit system;
       };
+    in
+    {
+      packages.${system}.xinux-plymouth-theme = pkgs.callPackage ./default.nix { };
+      defaultPackage.${system} = self.packages.${system}.xinux-plymouth-theme;
     };
 }
