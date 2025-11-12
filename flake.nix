14d5f19bbb2ac93bdd8af1e8f1661c920435fee6
BeMeritus <bemerituss@gmail.com>
2025-11-12 10:07:48 +0500

    chore: recreated

--- flake.nix
+++ flake.nix
@@ -1,33 +1,61 @@
+
 {
-  description = "Mac-style NixOS Plymouth Theme";
+  description = "Flake for adi1090x Plymouth themes (with optional selected themes)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    with flake-utils.lib; eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      packages = {
-        default = pkgs.callPackage ./package.nix {};
-        xinux = pkgs.callPackage ./package.nix {};
-      };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
 
-      devShells.default = pkgs.mkShell {
-        name = "plymouth-test";
-        packages = with pkgs; [
-          plymouth
-          (pkgs.writeShellScriptBin "show" (builtins.readFile ./showplaymouth.sh))
-        ];
-      };
-    })
-    // {
-      overlays.default = final: prev: {
-         xinux = final.callPackage ./package.nix {};
+
+      selectedThemes = [ "mac_style"]; # misol uchun
+      # selectedThemes = [ ]; # hammasini olish uchun
+
+
+      themeShas = import ./shas.nix;
+      knownThemes = builtins.attrNames themeShas;
+      selected =
+        if (selectedThemes == [ ]) then
+          knownThemes
+        else
+          let
+            unknown = pkgs.lib.subtractLists knownThemes selectedThemes;
+          in
+          if (unknown != [ ]) then
+            throw "Unknown theme(s): ${pkgs.lib.concatStringsSep " " unknown}"
+          else
+            selectedThemes;
+
+      srcs = pkgs.lib.lists.forEach selected (
+        name:
+        pkgs.fetchurl {
+          url = themeShas.${name}.url;
+          sha256 = themeShas.${name}.sha;
+        }
+      );
+    in {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "xinux-plymouth-theme";
+        version = "1.0";
+        inherit srcs;
+
+        nativeBuildInputs = [ pkgs.unzip ];
+
+        sourceRoot = ".";
+        unpackCmd = "tar xzf $curSrc";
+
+        installPhase = ''
+          mkdir -p $out/share/plymouth/themes
+          for theme in ${toString selected}; do
+            mv $theme $out/share/plymouth/themes/$theme
+          done
+          find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@/usr/@$out/@" {} \;
+        '';
+
       };
     };
 }
---
+++ shas.nix
@@ -0,0 +1,7 @@
+{
+  "abstract_ring" = {
+    url = "https://github.com/adi1090x/plymouth-themes/releases/download/v1.0/abstract_ring.tar.gz";
+    sha = "0hfrzm974nm8f237vzlyjjh4pikva3n7fqr1p7jmx04g857g4rjq";
+  };
+
+}
